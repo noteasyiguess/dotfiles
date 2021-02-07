c02d35e87255d6e028c746e70203cfe88c7f32a6
TheSimple <mantrshsingh@hotmail.com>
2021-02-08 05:07:33 +0530

    Added emacs dotfile

---
+++ Documents/emacs/ground-up/init.el
@@ -0,0 +1,18 @@
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font Mono" :height 130)
+(load-theme 'doom-gruvbox 't)
+(setq backup-directory-alist '(("." . "~/.cache/.saves"))
+      delete-old-versions t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
