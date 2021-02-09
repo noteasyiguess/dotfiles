e2202d268d07ddfc5be67b3e18fb7d78cca194ca
TheSimple <mantrshsingh@hotmail.com>
2021-02-09 20:38:04 +0530

    switched from irony to lsp for code completion

--- Documents/emacs/ground-up/init.el
+++ Documents/emacs/ground-up/init.el
@@ -15,11 +15,27 @@
 ;; Escape immediately quits
 (global-set-key (kbd "<escape>") 'keyboard-quit)
 
+;; Handy function
+(defun kill-all-buffers ()
+  (delete-other-windows)
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
+
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
 
 (use-package all-the-icons)
 (use-package magit)
+
+(use-package lsp-mode
+  :hook
+  (c++-mode . lsp-deferred)
+  (c-mode . lsp-deferred)
+  (objc-mode . lsp-deferred)
+  :commands (lsp lsp-deferred))
+;; (use-package lsp-ui
+;;   :config
+;;   (lsp-ui-doc-enable nil))
 
 (use-package counsel
   :bind
@@ -42,28 +58,29 @@
   ("C-c v" . ivy-push-view)
   ("C-c V" . ivy-pop-view)
   :config (ivy-mode 1))
+(use-package lsp-ivy)
 
 (use-package company
   :config
-  (setq company-minimum-prefix-length 5
-	company-idle-delay 0.500))
+  (setq company-minimum-prefix-length 2)
+	company-idle-delay 0.100)
 (add-hook 'after-init-hook 'global-company-mode)
 
-(use-package irony
-  :config
-    (add-hook 'c++-mode-hook 'irony-mode)
-    (add-hook 'c-mode-hook 'irony-mode)
-    (add-hook 'objc-mode-hook 'irony-mode)
-    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
-(use-package company-irony)
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
+;; (use-package irony
+;;   :config
+;;     (add-hook 'c++-mode-hook 'irony-mode)
+;;     (add-hook 'c-mode-hook 'irony-mode)
+;;     (add-hook 'objc-mode-hook 'irony-mode)
+;;     (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+;; (use-package company-irony)
+;; (eval-after-load 'company
+;;   '(add-to-list 'company-backends 'company-irony))
 
 (use-package flycheck
   :init (global-flycheck-mode))
-(use-package flycheck-irony)
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+;; (use-package flycheck-irony)
+;; (eval-after-load 'flycheck
+;;   '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
 (use-package evil
   :init
@@ -74,6 +91,8 @@
   :after evil
   :config
   (evil-collection-init))
+(use-package evil-nerd-commenter)
+(global-set-key (kbd "C-c l") 'evilnc-comment-or-uncomment-lines)
 
 (use-package tree-sitter)
 ;;  :config
@@ -88,12 +107,49 @@
   :config
   (doom-themes-org-config))
 (use-package kaolin-themes)
+(use-package gruvbox-theme)
+
+(use-package doom-modeline
+  :hook (after-init . doom-modeline-mode))
 
 (load-theme 'doom-gruvbox t)
-(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font Mono" :height 140)
+
+;; When running in daemon mode, the font is not set since there is no frame
+(defun my-frame-init ()
+  (set-face-attribute 'default nil :font "GoMono Nerd Font Mono" :height 150)
+  (set-face-attribute 'variable-pitch nil :font "Noto Sans" :height 150)
+  (setq doom-modeline-icon t))
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+	      (lambda (frame)
+		(select-frame frame)
+		(my-frame-init)))
+  (my-frame-init))
+
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-message t)
+(setq initial-buffer-choice nil)
+
+;; Long line performance improvements
+(setq bidi-paragraph-direction 'left-to-right)
+(setq bidi-inhibit-bpa t)
+(global-so-long-mode 1)
+
+;; Relative line numbers
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
+
+;; Disable line folding
+(set-default 'truncate-lines t)
+
+;; Match the other bracket pair
+(show-paren-mode 1)
 
 ;; Automatically pair parens
 (electric-pair-mode 1)
+
+;; Disable that annoying blinking cursor
+(blink-cursor-mode -1)
 
 ;; All backup files are stored in one direcctory
 ;;(setq backup-directory-alist '(("." . "~/.cache/.saves"))
@@ -114,47 +170,14 @@
       version-control nil    ; Use version numbers on backups
       kept-new-versions 2    ; keep some new versions
       kept-old-versions 2)   ; and some old ones, too
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
  '(custom-safe-themes
-   '("be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "5b809c3eae60da2af8a8cfba4e9e04b4d608cb49584cb5998f6e4a1c87c057c4" "0fe24de6d37ea5a7724c56f0bb01efcbb3fe999a6e461ec1392f3c3b105cc5ac" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "c9415c9f5a5ed67914d1d64a0ea7d743ef93516f1f2c8501bc5ffb87af2066d3" "7236acec527d58086ad2f1be6a904facc9ca8bf81ed1c19098012d596201b3f1" "4e9e56ec06ede9857c876fea2c44b75dd360cd29a7fe927b706c45f804f7beff" "a4395e069de3314001de4e139d6a3b1a83dcf9c3fdc68ee7b13eef6c2cba4ae3" "8f54cfa3f010d83d782fbcdc3a34cdc9dfe23c8515d87ba22d410c033160ad7e" "7e5d400035eea68343be6830f3de7b8ce5e75f7ac7b8337b5df492d023ee8483" "b9e406b52f60a61c969f203958f406fed50b5db5ac16c127b86bbddd9d8444f7" "d9a28a009cda74d1d53b1fbd050f31af7a1a105aa2d53738e9aa2515908cac4c" "73320ccc14ab4987fe2e97cfd810b33a1f4a115f5f056c482c3d38a4429e1705" "620b9018d9504f79344c8ef3983ea4e83d209b46920f425240889d582be35881" "0c6a36393d5782839b88e4bf932f20155cb4321242ce75dc587b4f564cb63d90" "7d708f0168f54b90fc91692811263c995bebb9f68b8b7525d0e2200da9bc903c" "6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "95d0ed21bb0e919be7687a25ad59a1c2c8df78cbe98c9e369d44e65bfd65b167" "aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "cae81b048b8bccb7308cdcb4a91e085b3c959401e74a0f125e7c5b173b916bf9" "2c49d6ac8c0bf19648c9d2eabec9b246d46cb94d83713eaae4f26b49a8183fc4" "c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" "ca70827910547eb99368db50ac94556bbd194b7e8311cfbdbdcad8da65e803be" "e3c64e88fec56f86b49dcdc5a831e96782baf14b09397d4057156b17062a8848" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" "0a41da554c41c9169bdaba5745468608706c9046231bbbc0d155af1a12f32271" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" default))
- '(fci-rule-color "#405A61")
- '(ivy-mode t)
- '(jdee-db-active-breakpoint-face-colors (cons "#073642" "#268bd2"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#073642" "#859900"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#073642" "#56697A"))
- '(objed-cursor-color "#dc322f")
- '(pdf-view-midnight-colors (cons "#839496" "#002b36"))
- '(rustic-ansi-faces
-   ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
- '(vc-annotate-background "#002b36")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#859900")
-    (cons 40 "#959300")
-    (cons 60 "#a58e00")
-    (cons 80 "#b58900")
-    (cons 100 "#bc7407")
-    (cons 120 "#c35f0e")
-    (cons 140 "#cb4b16")
-    (cons 160 "#cd4439")
-    (cons 180 "#d03d5d")
-    (cons 200 "#d33682")
-    (cons 220 "#d63466")
-    (cons 240 "#d9334a")
-    (cons 260 "#dc322f")
-    (cons 280 "#ba3f41")
-    (cons 300 "#994d54")
-    (cons 320 "#775b67")
-    (cons 340 "#405A61")
-    (cons 360 "#405A61")))
- '(vc-annotate-very-old-color nil))
+   '("7236acec527d58086ad2f1be6a904facc9ca8bf81ed1c19098012d596201b3f1" "4e9e56ec06ede9857c876fea2c44b75dd360cd29a7fe927b706c45f804f7beff" "7e5d400035eea68343be6830f3de7b8ce5e75f7ac7b8337b5df492d023ee8483" "b9e406b52f60a61c969f203958f406fed50b5db5ac16c127b86bbddd9d8444f7" "d9a28a009cda74d1d53b1fbd050f31af7a1a105aa2d53738e9aa2515908cac4c" "73320ccc14ab4987fe2e97cfd810b33a1f4a115f5f056c482c3d38a4429e1705" "78c01e1b7f3dc9e47bdd48f74dc98dc1a345c291f83b68ac8a1b40191f24d658" "0c6a36393d5782839b88e4bf932f20155cb4321242ce75dc587b4f564cb63d90" "aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "620b9018d9504f79344c8ef3983ea4e83d209b46920f425240889d582be35881" "d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" "83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" default))
+ '(default-input-method "devanagari-itrans"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
